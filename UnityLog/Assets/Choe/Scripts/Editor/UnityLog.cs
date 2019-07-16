02cc9faf5a5c8856ffd3e9c8a01df14191995eda
ch.choi <ccg.1120@daum.net>
2019-07-16 17:21:35 +0900

    1. 저장폴더 생성 기능 추가

--- UnityLog/Assets/Choe/Scripts/Editor/UnityLog.cs
+++ UnityLog/Assets/Choe/Scripts/Editor/UnityLog.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+
 
 public class UnityLog : Editor {
 
+    private static string m_LogSaveFolder = "Logs";
+    private static string m_LogSavePath = string.Empty;
 	
     public UnityLog()
     { 
+
         Debug.Log("UnityLog ");
     }
-        
 
-	
+    [InitializeOnLoadMethod]
+    public static void UnityLogInit()
+    {
+        Debug.Log("UnityLogInit");
+        Check_SaveLogFolder();
+    }
+
+    private static void Check_SaveLogFolder()
+    {
+        m_LogSavePath = Path.Combine(Directory.GetCurrentDirectory(), m_LogSaveFolder);
+
+        DirectoryInfo info = new DirectoryInfo(m_LogSavePath);
+        if(!info.Exists)
+        {
+            Debug.LogError("Create file path !!");
+            info.Create();
+        }
+    }
+
+
+
 }
