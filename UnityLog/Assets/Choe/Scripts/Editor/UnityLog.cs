fb29301c963fb5d9ebce952213198cc03c96d5a8
ch.choi <ccg.1120@daum.net>
2019-07-16 17:10:09 +0900

    1. project commit

---
+++ UnityLog/Assets/Choe/Scripts/Editor/UnityLog.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UnityLog : Editor {
+
+	
+    public UnityLog()
+    { 
+        Debug.Log("UnityLog ");
+    }
+        
+
+	
+}
